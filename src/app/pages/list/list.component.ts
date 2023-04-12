8ac46ba229301ad72fef16794c9127c500caf180
Oleksandr Havrilutsa <alex.mercer.324110@gmail.com>
2023-04-12 18:41:58 +0300

    ok

--- src/app/pages/list/list.component.ts
+++ src/app/pages/list/list.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core'
 import { Question, QuestionType } from '../../interfaces/question'
 import { QuestionService } from '../../services/question.service'
 import { BehaviorSubject } from 'rxjs'
-import { FormGroup } from '@angular/forms'
 
 @Component({
   selector: 'app-list',
@@ -13,21 +12,14 @@ export class ListComponent {
   QuestionType = QuestionType
   unansweredQuestions$: BehaviorSubject<Question[]>
   answeredQuestions$: BehaviorSubject<Question[]>
-  answerForms: FormGroup[] = []
 
   constructor(private questionService: QuestionService) {
     this.unansweredQuestions$ = this.questionService.unansweredQuestions$
     this.answeredQuestions$ = this.questionService.answeredQuestions$
 
-    for (let unansweredQuestion of this.unansweredQuestions$.value) {
-      if (!unansweredQuestion.answerOptions) return
+  }
 
-      this.answerForms.push(
-        new FormGroup({}))
-      // @ts-ignore
-      for (let option of unansweredQuestion.answerOptions) {
-        console.log(option)
-      }
-    }
+  answerQuestion(question: Question, text: string) {
+    console.log(question, text)
   }
 }
